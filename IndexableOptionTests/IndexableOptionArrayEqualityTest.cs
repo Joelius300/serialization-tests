0fee0f4ecc7754b2ed3120ed7d3e751d55e4208a
Joelius300 <52202086+Joelius300@users.noreply.github.com>
2019-09-01 21:20:47 +0200

    Add equality tests

---
+++ IndexableOptionTests/IndexableOptionArrayEqualityTest.cs
@@ -0,0 +1,105 @@
+﻿using IndexableOptionTests.TestClasses;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace IndexableOptionTests
+{
+    public class IndexableOptionArrayEqualityTest
+    {
+        [Fact]
+        public void Reference_Equal_Equatable()
+        {
+            // Arrange
+            int[] ints = { 0, 1, 4 };
+            IndexableOption<int> first = ints;
+            IndexableOption<int> second = ints;
+
+            // Act
+            bool areEqual = first.Equals(second);
+
+            // Assert
+            Assert.True(areEqual);
+        }
+
+        [Fact]
+        public void Reference_Equal_Equals()
+        {
+            // Arrange
+            int[] ints = { 0, 1, 4 };
+            IndexableOption<int> first = ints;
+            IndexableOption<int> second = ints;
+
+            // Act
+            bool areEqual = first.Equals((object)second);
+
+            // Assert
+            Assert.True(areEqual);
+        }
+
+        [Fact]
+        public void Reference_Equal_Equals_Wrapped()
+        {
+            // Arrange
+            int[] ints = { 0, 1, 4 };
+            IndexableOption<int> first = ints;
+
+            // Act
+            bool areEqual = first.Equals(ints);
+
+            // Assert
+            Assert.True(areEqual);
+        }
+
+        [Fact]
+        public void Sequence_Equal_Equatable()
+        {
+            // Arrange
+            int[] ints1 = { 0, 1, 4 };
+            int[] ints2 = { 0, 1, 4 };
+
+            IndexableOption<int> first = ints1;
+            IndexableOption<int> second = ints2;
+
+            // Act
+            bool areEqual = first.Equals(second);
+
+            // Assert
+            Assert.True(areEqual);
+        }
+
+        [Fact]
+        public void Sequence_Equal_Equals()
+        {
+            // Arrange
+            int[] ints1 = { 0, 1, 4 };
+            int[] ints2 = { 0, 1, 4 };
+
+            IndexableOption<int> first = ints1;
+            IndexableOption<int> second = ints2;
+
+            // Act
+            bool areEqual = first.Equals((object)second);
+
+            // Assert
+            Assert.True(areEqual);
+        }
+
+        [Fact]
+        public void Sequence_Equal_Equals_Wrapped()
+        {
+            // Arrange
+            int[] ints1 = { 0, 1, 4 };
+            int[] ints2 = { 0, 1, 4 };
+
+            IndexableOption<int> first = ints1;
+
+            // Act
+            bool areEqual = first.Equals(ints2);
+
+            // Assert
+            Assert.True(areEqual);
+        }
+    }
+}
---
+++ IndexableOptionTests/IndexableOptionValueEqualityTest.cs
@@ -0,0 +1,55 @@
+﻿using IndexableOptionTests.TestClasses;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace IndexableOptionTests
+{
+    public class IndexableOptionValueEqualityTest
+    {
+        [Fact]
+        public void Value_Equal_Equatable()
+        {
+            // Arrange
+            int val = 0;
+            IndexableOption<int> first = val;
+            IndexableOption<int> second = val;
+
+            // Act
+            bool areEqual = first.Equals(second);
+
+            // Assert
+            Assert.True(areEqual);
+        }
+
+        [Fact]
+        public void Value_Equal_Equals()
+        {
+            // Arrange
+            int val = 0;
+            IndexableOption<int> first = val;
+            IndexableOption<int> second = val;
+
+            // Act
+            bool areEqual = first.Equals((object)second);
+
+            // Assert
+            Assert.True(areEqual);
+        }
+
+        [Fact]
+        public void Value_Equal_Equals_Wrapped()
+        {
+            // Arrange
+            int val = 0;
+            IndexableOption<int> first = val;
+
+            // Act
+            bool areEqual = first.Equals(val);
+
+            // Assert
+            Assert.True(areEqual);
+        }
+    }
+}
